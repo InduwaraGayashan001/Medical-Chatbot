c0d26553794938c0b27c9096baef8832bd7a26c1
InduwaraGayashan001 <induwaragayashan@gmail.com>
2025-07-01 18:47:03 +0530

    Initialize project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Medical Chatbot',
+    version='0.0.0',
+    author='Induwara Gayashan',
+    author_email='induwaragayashan@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
